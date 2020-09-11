df45009d8c3ee1e4094d097e93eeb259cf875a5b
evanwashere <evan@tsu.sh>
2020-09-11 15:13:54 +0200

    lint

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ for await (const dir of Deno.readDir('./src')) {
   const wasm = encode(await Deno.readFile(`./src/${dir.name}/${dir.name}.wasm`));
   const js = Deno.core.decode(await Deno.readFile(`./src/${dir.name}/${dir.name}.js`));
 
-  await Deno.mkdir(`./target/${dir.name}`).catch(() => {});
+  await Deno.mkdir(`./target/${dir.name}`).catch(() => { });
   Deno.writeFile(`./target/${dir.name}/node.mjs`, Deno.core.encode(js.replace(key, `Buffer.from('${wasm}', 'base64').buffer`)));
   Deno.writeFile(`./target/${dir.name}/deno.js`, Deno.core.encode(js.replace(key, `Uint8Array.from(atob('${wasm}'), char => char.codePointAt(0))`)));
 }
