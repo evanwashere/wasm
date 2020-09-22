8d8f0fa3c291491e2fdc4bb474bba814004165c0
evanwashere <evan@tsu.sh>
2020-09-22 13:07:51 +0200

    add crypto

---
+++ src/crypto/crypto.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+
+const hash = std.crypto.hash;
+const allocator = std.heap.page_allocator;
+
+export fn free(ptr: u32, len: usize) void {
+  allocator.free(@intToPtr([*]u8, ptr)[0..len]);
+}
+
+export fn malloc(len: usize) u32 {
+  return @ptrToInt((allocator.alloc(u8, len) catch return 0).ptr);
+}
+
+
+export fn md5(ptr: u32, len: usize) u32 {
+  return hash_to(hash.Md5, 16, ptr, len);
+}
+
+
+export fn sha1(ptr: u32, len: usize) u32 {
+  return hash_to(hash.Sha1, 20, ptr, len);
+}
+
+export fn sha2_256(ptr: u32, len: usize) u32 {
+  return hash_to(hash.sha2.Sha256, 32, ptr, len);
+}
+
+export fn sha3_256(ptr: u32, len: usize) u32 {
+  return hash_to(hash.sha3.Sha3_256, 32, ptr, len);
+}
+
+
+export fn blake3(ptr: u32, len: usize) u32 {
+  return hash_to(hash.Blake3, 32, ptr, len);
+}
+
+export fn blake2_s256(ptr: u32, len: usize) u32 {
+  return hash_to(hash.blake2.Blake2s256, 32, ptr, len);
+}
+
+export fn blake2_b256(ptr: u32, len: usize) u32 {
+  return hash_to(hash.blake2.Blake2b256, 32, ptr, len);
+}
+
+
+fn hash_to(comptime h: type, comptime out_len: u8, ptr: u32, len: usize) u32 {
+  const in = @intToPtr([*]u8, ptr)[0..len];
+
+  defer allocator.free(in);
+  errdefer allocator.free(in);
+  const out = allocator.alloc(u8, out_len) catch return 0;
+
+  h.hash(in, out[0..], .{});
+  return @ptrToInt(out.ptr);
+}
--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import * as zlib from './target/zlib/deno.js';
 import * as oxipng from './target/oxipng/deno.js';
 import * as search from './target/search/deno.js';
 import * as snappy from './target/snappy/deno.js';
+import * as zcrypto from './target/crypto/deno.js';
 import * as fasteval from './target/fasteval/deno.js';
 
 // import fs from 'fs';
@@ -12,6 +13,7 @@ import * as fasteval from './target/fasteval/deno.js';
 // import * as oxipng from './target/oxipng/node.mjs;
 // import * as search from './target/search/node.mjs';
 // import * as snappy from './target/snappy/node.mjs';
+// import * as zcrypto from './target/crypto/node.mjs';
 // import * as fasteval from './target/fasteval/node.mjs';
 
 // const Deno = {
@@ -20,12 +22,25 @@ import * as fasteval from './target/fasteval/deno.js';
 //   }
 // }
 
+const skip = {
+  lz4: false,
+  zlib: false,
+  snappy: false,
+  search: false,
+  oxipng: false,
+  crypto: false,
+  fasteval: false,
+}
+
 fasteval: {
+  if (skip.fasteval) break fasteval;
+
   let x;
   console.log(`${'5' === (x = fasteval.evaluate('2 + 3'))}: ${x}`);
 }
 
 lz4: {
+  if (skip.lz4) break lz4;
   let s = `hi hello ${'a'.repeat(200)}`;
   const a = lz4.compress(Deno.core.encode(s));
   console.log(a, s === Deno.core.decode(lz4.decompress(a)));
@@ -33,6 +48,7 @@ lz4: {
 
 
 snappy: {
+  if (skip.snappy) break snappy;
   let s = `hi hello ${'a'.repeat(200)}`;
   const a = snappy.compress(Deno.core.encode(s));
   const b = snappy.compress_raw(Deno.core.encode(s));
@@ -42,6 +58,7 @@ snappy: {
 }
 
 zlib: {
+  if (skip.zlib) break zlib;
   let s = `hi hello ${'a'.repeat(200)}`;
   const a = zlib.compress(Deno.core.encode(s), 0);
   const b = zlib.compress_raw(Deno.core.encode(s), 1);
@@ -53,6 +70,7 @@ zlib: {
 }
 
 search: {
+  if (skip.search) break search;
   const index = new search.Index('en');
 
   index.add(1, 'hello');
@@ -66,6 +84,8 @@ search: {
 }
 
 oxipng: {
+  if (skip.oxipng) break oxipng;
+
   let x;
   let i = 1
   const image = new Uint8Array(await(await fetch('https://tsu.sh/boncg4b59kq.png')).arrayBuffer());
@@ -76,4 +96,16 @@ oxipng: {
   Deno.writeFile(`./test${i}.png`, x = oxipng.optimize(image, i)); console.log(`${image.length - x.length} bytes saved with level ${i++}`);
   Deno.writeFile(`./test${i}.png`, x = oxipng.optimize(image, i)); console.log(`${image.length - x.length} bytes saved with level ${i++}`);
   Deno.writeFile(`./test${i}.png`, x = oxipng.optimize(image, i)); console.log(`${image.length - x.length} bytes saved with level ${i++}`);
+}
+
+crypto: {
+  if (skip.crypto) break crypto;
+  const bytes = new Uint8Array(1024);
+  console.log(zcrypto.hash.md5(bytes));
+  console.log(zcrypto.hash.sha1(bytes));
+  console.log(zcrypto.hash.blake3(bytes));
+  console.log(zcrypto.hash.sha2.sha256(bytes));
+  console.log(zcrypto.hash.sha3.sha256(bytes));
+  console.log(zcrypto.hash.blake2.s256(bytes));
+  console.log(zcrypto.hash.blake2.b256(bytes));
 }
