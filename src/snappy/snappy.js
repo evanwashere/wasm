9d9119592cb088fcc5de1169fa798003757d3683
evanwashere <evan@tsu.sh>
2021-01-02 07:07:13 -0500

    snappy: update bindings

--- src/snappy/snappy.js
+++ src/snappy/snappy.js
@@ -1,113 +1,89 @@
 import { Pool } from 'https://jspm.dev/structurae@3.3.0'; // !deno
 
 let wasm;
+const streams = new Map; // !deno
+const pool = new Pool(256 * 1024); // !deno
 
 {
   const module = new WebAssembly.Module(WASM_BYTES);
   const instance = new WebAssembly.Instance(module, {
-    __wbindgen_placeholder__: {
-      __wbindgen_throw() { throw new Error('snappy: wtf') },
-      __wbg_decompressok_e508b22a200a6458(ptr, len) { decompress_resolve(ptr_to_u8array(ptr, len)) },
-      __wbg_pushtostream_4d6316a40e73e31f(id, ptr, len) { push_to_stream(id >>> 0, ptr_to_u8array(ptr, len)) },
+    env: {
+      push_to_stream(id, ptr) {
+        streams.get(id).cb(mem.u8(ptr, mem.length()).slice()); // !deno
+      },
     },
   });
 
   wasm = instance.exports;
 }
 
-const streams = new Map; // !deno
-const pool = new Pool(256 * 1024); // !deno
-let u8array_ref = new Uint8Array(wasm.memory.buffer);
-let i32array_ref = new Int32Array(wasm.memory.buffer);
+class mem {
+  static alloc(size) { return wasm.walloc(size); }
+  static free(ptr, size) { return wasm.wfree(size, ptr); }
+  static u8(ptr, size) { return new Uint8Array(wasm.memory.buffer, ptr, size); }
+  static u32(ptr, size) { return new Uint32Array(wasm.memory.buffer, ptr, size); }
+  static length() { return new Uint32Array(wasm.memory.buffer, wasm.cur_len.value, 1)[0]; }
 
-function u8array() {
-  return u8array_ref.buffer === wasm.memory.buffer ? u8array_ref : (u8array_ref = new Uint8Array(wasm.memory.buffer));
-}
-
-function i32array() {
-  return i32array_ref.buffer === wasm.memory.buffer ? i32array_ref : (i32array_ref = new Int32Array(wasm.memory.buffer));
-}
-
-function ptr_to_u8array(ptr, len) {
-  return u8array().subarray(ptr, ptr + len);
-}
-
-function u8array_to_ptr(buffer) {
-  const ptr = wasm.__wbindgen_malloc(buffer.length);
-
-  return (u8array().set(buffer, ptr), ptr);
-}
-
-let decompress_value = null;
-let decompress_callback = null;
-function push_to_stream(id, buffer) { streams.get(id).cb(buffer.slice()); } // !deno
-function decompress_resolve(buffer) { decompress_value = !decompress_callback ? buffer.slice() : decompress_callback(buffer); }
-
-export function decompress(buffer) {
-  const ptr = u8array_to_ptr(buffer);
-  if (0 !== wasm.decompress(ptr, buffer.length)) throw new Error('snappy: failed to decompress');
-
-  const ref = decompress_value;
-  return (decompress_value = null, ref);
-}
-
-export function decompress_raw(buffer) {
-  const ptr = u8array_to_ptr(buffer);
-  if (0 !== wasm.decompress_raw(ptr, buffer.length)) throw new Error('snappy: failed to decompress (raw)');
-
-  const ref = decompress_value;
-  return (decompress_value = null, ref);
-}
-
-export function decompress_with(buffer, transform) {
-  decompress_callback = transform;
-  const ptr = u8array_to_ptr(buffer);
-  if (0 !== wasm.decompress(ptr, buffer.length)) throw new Error('snappy: failed to decompress');
-
-  decompress_callback = null;
-  const ref = decompress_value;
-  return (decompress_value = null, ref);
-}
-
-export function decompress_raw_with(buffer, transform) {
-  decompress_callback = transform;
-  const ptr = u8array_to_ptr(buffer);
-  if (0 !== wasm.decompress_raw(ptr, buffer.length)) throw new Error('snappy: failed to decompress (raw)');
-
-  decompress_callback = null;
-  const ref = decompress_value;
-  return (decompress_value = null, ref);
+  static copy_and_free(ptr, size) {
+    let slice = mem.u8(ptr, size).slice();
+    return (wasm.wfree(size, ptr), slice);
+  }
 }
 
 export function compress(buffer) {
-  const ptr = u8array_to_ptr(buffer);
-  const out = wasm.__wbindgen_export_0.value -= 16;
-
-  wasm.compress(out, ptr, buffer.length);
-
-  const i32 = i32array();
-  const offset = i32[out / 4];
-  const offset_length = i32[1 + out / 4];
-  const slice = ptr_to_u8array(offset, offset_length).slice();
-
-  wasm.__wbindgen_free(offset, offset_length);
-  return (wasm.__wbindgen_export_0.value += 16, slice);
+  const ptr = mem.alloc(buffer.length);
+  mem.u8(ptr, buffer.length).set(buffer);
+  return mem.copy_and_free(wasm.compress(buffer.length, ptr), mem.length());
 }
 
 export function compress_raw(buffer) {
-  const ptr = u8array_to_ptr(buffer);
-  const out = wasm.__wbindgen_export_0.value -= 16;
-
-  wasm.compress_raw(out, ptr, buffer.length);
-
-  const i32 = i32array();
-  const offset = i32[out / 4];
-  const offset_length = i32[1 + out / 4];
-  const slice = ptr_to_u8array(offset, offset_length).slice();
-
-  wasm.__wbindgen_free(offset, offset_length);
-  return (wasm.__wbindgen_export_0.value += 16, slice);
+  const ptr = mem.alloc(buffer.length);
+  mem.u8(ptr, buffer.length).set(buffer);
+  return mem.copy_and_free(wasm.compress_raw(buffer.length, ptr), mem.length());
 }
+
+export function decompress(buffer) {
+  const ptr = mem.alloc(buffer.length);
+  mem.u8(ptr, buffer.length).set(buffer);
+  const x = wasm.decompress(buffer.length, ptr);
+  if (0 === x) throw new Error('snappy: failed to decompress');
+
+  return mem.copy_and_free(x, mem.length());
+}
+
+export function decompress_raw(buffer) {
+  const ptr = mem.alloc(buffer.length);
+  mem.u8(ptr, buffer.length).set(buffer);
+  const x = wasm.decompress_raw(buffer.length, ptr);
+  if (0 === x) throw new Error('snappy: failed to decompress (raw)');
+
+  return mem.copy_and_free(x, mem.length());
+}
+
+export function decompress_with(buffer, transform) {
+  const ptr = mem.alloc(buffer.length);
+  mem.u8(ptr, buffer.length).set(buffer);
+  const x = wasm.decompress(buffer.length, ptr);
+  if (0 === x) throw new Error('snappy: failed to decompress');
+
+  const u8 = mem.u8(x, mem.length());
+
+  const value = transform(u8);
+  return (mem.free(x, u8.length), value);
+}
+
+export function decompress_raw_with(buffer, transform) {
+  const ptr = mem.alloc(buffer.length);
+  mem.u8(ptr, buffer.length).set(buffer);
+  const x = wasm.decompress_raw(buffer.length, ptr);
+  if (0 === x) throw new Error('snappy: failed to decompress (raw)');
+
+  const u8 = mem.u8(x, mem.length());
+
+  const value = transform(u8);
+  return (mem.free(x, u8.length), value);
+}
+
 
 // !
 export class CompressionStream {
@@ -132,7 +108,7 @@ export class Compressor {
     this.cb = callback;
     this.id = pool.get();
     streams.set(this.id, this);
-    this.ptr = wasm.compressor_new(this.id);
+    console.log(this.ptr = wasm.compressor_new(this.id));
   }
 
   flush() {
@@ -142,12 +118,13 @@ export class Compressor {
   free() {
     pool.free(this.id);
     streams.delete(this.id);
-    wasm.__wbg_compressor_free(this.ptr);
+    wasm.compressor_free(this.ptr);
   }
 
   write(buffer) {
-    const ptr = u8array_to_ptr(buffer);
-    return (wasm.compressor_write(this.ptr, ptr, buffer.length), wasm.__wbindgen_free(ptr, buffer.length));
+    const ptr = mem.alloc(buffer.length);
+    mem.u8(ptr, buffer.length).set(buffer);
+    wasm.compressor_write(this.ptr, buffer.length, ptr);
   }
 }
 // !deno
--- src/snappy/snappy.rs
+++ src/snappy/snappy.rs
@@ -1,4 +1,5 @@
-use wasm_bindgen::prelude::*;
+mod mem;
+use mem::UnsafeUnwrap;
 use std::io::{self, Read, Write};
 
 #[allow(non_camel_case_types)]
@@ -6,64 +7,72 @@ struct js_callback {
   id: usize,
 }
 
-#[wasm_bindgen]
 pub struct Compressor {
   writer: snap::write::FrameEncoder<js_callback>,
 }
 
-
-#[wasm_bindgen]
-extern "C" {
-  fn decompress_ok(buffer: &[u8]);
-  fn push_to_stream(id: usize, buffer: &[u8]);
-}
-
-
-#[wasm_bindgen]
-pub fn compress_raw(buffer: &[u8]) -> Vec<u8> {
-  snap::raw::Encoder::new().compress_vec(buffer).unwrap()
-}
-
-#[wasm_bindgen]
-pub fn compress(buffer: &[u8]) -> Vec<u8> {
-  let mut w = snap::write::FrameEncoder::new(vec![]);
-  w.write_all(buffer).unwrap(); w.into_inner().unwrap()
-}
-
-#[wasm_bindgen]
-pub fn decompress_raw(buffer: &[u8]) -> u8 {
-  match snap::raw::Decoder::new().decompress_vec(buffer) {
-    Err(_) => return 1,
-    Ok(buf) => decompress_ok(&buf),
-  }
-
-  0
-}
-
-#[wasm_bindgen]
-pub fn decompress(buffer: &[u8]) -> u8 {
-  let mut buf = vec![];
-  match snap::read::FrameDecoder::new(buffer).read_to_end(&mut buf) {
-    Err(_) => return 1,
-    Ok(_) => decompress_ok(&buf),
-  }
-
-  0
-}
-
-
 impl io::Write for js_callback {
   fn flush(&mut self) -> io::Result<()> { Ok(()) }
-  fn write(&mut self, buffer: &[u8]) -> io::Result<usize> { push_to_stream(self.id, buffer); Ok(buffer.len()) }
+  fn write(&mut self, buffer: &[u8]) -> io::Result<usize> {
+    unsafe { push_to_stream(self.id, mem::ptr(&buffer)); }
+
+    Ok(buffer.len())
+  }
 }
 
-#[wasm_bindgen]
 impl Compressor {
-  #[wasm_bindgen(constructor)]
   pub fn new(id: usize) -> Self {
     Self { writer: snap::write::FrameEncoder::new(js_callback { id }) }
   }
 
-  pub fn flush(&mut self) { self.writer.flush().unwrap() }
-  pub fn write(&mut self, buffer: &mut [u8]) { self.writer.write_all(buffer).unwrap() }
+  pub fn flush(&mut self) { unsafe { self.writer.flush().unsafe_unwrap(); } }
+  pub fn write(&mut self, buffer: &mut [u8]) { unsafe { self.writer.write_all(buffer).unsafe_unwrap(); } }
+}
+
+
+extern "C" {
+  fn push_to_stream(id: usize, ptr: *const u8);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn compressor_flush(ptr: *mut Compressor) { (*ptr).flush(); }
+
+#[no_mangle]
+pub unsafe extern "C" fn compressor_free(ptr: *mut Compressor) { Box::from_raw(ptr); }
+
+#[no_mangle]
+pub extern "C" fn compressor_new(id: usize) -> *mut Compressor { Box::into_raw(Box::new(Compressor::new(id))) }
+
+#[no_mangle]
+pub unsafe extern "C" fn compressor_write(ptr: *mut Compressor, size: usize, bptr: *mut u8) { (*ptr).write(&mut mem::load(size, bptr)); }
+
+#[no_mangle]
+pub unsafe extern "C" fn compress_raw(size: usize, ptr: *mut u8) -> *const u8 {
+  return mem::store(&mut snap::raw::Encoder::new().compress_vec(&mem::load(size, ptr)).unsafe_unwrap());
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn compress(size: usize, ptr: *mut u8) -> *const u8 {
+  let mut w = snap::write::FrameEncoder::new(vec![]);
+
+  w.write_all(&mem::load(size, ptr)).unsafe_unwrap();
+  return mem::store(&mut w.into_inner().unsafe_unwrap());
+}
+
+#[no_mangle]
+pub extern "C" fn decompress_raw(size: usize, ptr: *mut u8) -> *const u8 {
+  match snap::raw::Decoder::new().decompress_vec(&mem::load(size, ptr)) {
+    Err(_) => 0 as *const u8,
+    Ok(mut buf) => mem::store(&mut buf),
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn decompress(size: usize, ptr: *mut u8) -> *const u8 {
+  let mut buf = vec![];
+
+  match snap::read::FrameDecoder::new(&*mem::load(size, ptr)).read_to_end(&mut buf) {
+    Err(_) => 0 as *const u8,
+    Ok(_) => mem::store(&mut buf),
+  }
 }
