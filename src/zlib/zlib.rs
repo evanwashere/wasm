9f5ea6385435be3d6bd17da7ab151f99b2d45ffe
evanwashere <evan@tsu.sh>
2021-02-14 17:56:38 -0500

    zlib: use minizoxide for decompression

--- src/zlib/zlib.rs
+++ src/zlib/zlib.rs
@@ -30,44 +30,29 @@ pub unsafe extern "C" fn compress_raw(ptr: *mut u8, size: usize, level: u32) -> 
   return mem::store(&mut buf);
 }
 
+
 #[no_mangle]
-pub extern "C" fn decompress(ptr: *mut u8, size: usize) -> *const u8 {
-  let mut buf = vec![];
-
-  {
-    let mut w = flate2::write::ZlibDecoder::new(&mut buf);
-
-    match w.write_all(&mem::load(ptr, size)) {
-      Ok(_) => {},
-      Err(_) => return 0 as *const u8,
-    };
-
-    match w.flush() {
-      Ok(_) => {},
-      Err(_) => return 0 as *const u8,
-    }
+pub extern "C" fn decompress_raw(ptr: *mut u8, size: usize, limit: usize) -> *const u8 {
+  let result = match limit {
+    0 => miniz_oxide::inflate::decompress_to_vec(&mem::load(ptr, size)),
+    _ => miniz_oxide::inflate::decompress_to_vec_with_limit(&mem::load(ptr, size), limit),
   };
 
-  return mem::store(&mut buf);
+  match result {
+    Err(_) => 0 as *const u8,
+    Ok(mut buf) => mem::store(&mut buf),
+  }
 }
 
 #[no_mangle]
-pub extern "C" fn decompress_raw(ptr: *mut u8, size: usize) -> *const u8 {
-  let mut buf = vec![];
-
-  {
-    let mut w = flate2::write::DeflateDecoder::new(&mut buf);
-
-    match w.write_all(&mem::load(ptr, size)) {
-      Ok(_) => {},
-      Err(_) => return 0 as *const u8,
-    };
-
-    match w.flush() {
-      Ok(_) => {},
-      Err(_) => return 0 as *const u8,
-    }
+pub extern "C" fn decompress(ptr: *mut u8, size: usize, limit: usize) -> *const u8 {
+  let result = match limit {
+    0 => miniz_oxide::inflate::decompress_to_vec_zlib(&mem::load(ptr, size)),
+    _ => miniz_oxide::inflate::decompress_to_vec_zlib_with_limit(&mem::load(ptr, size), limit),
   };
 
-  return mem::store(&mut buf);
+  match result {
+    Err(_) => 0 as *const u8,
+    Ok(mut buf) => mem::store(&mut buf),
+  }
 }
