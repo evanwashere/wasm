3a5186c7a522e3509b35b5fa8ec518c2eb50993b
evanwashere <evan@tsu.sh>
2021-06-12 09:46:37 -0400

    opus: finalize encoder bindings and export ctl

--- src/opus/opus.js
+++ src/opus/opus.js
@@ -5,7 +5,7 @@ let wasm;
 {
   const module = new WebAssembly.Module(WASM_BYTES);
   const instance = new WebAssembly.Instance(module, {
-    wasi_snapshot_preview1: { fd_seek() {}, fd_write() {}, fd_close() {}, proc_exit() {} },
+    wasi_snapshot_preview1: { fd_seek() { }, fd_write() { }, fd_close() { }, proc_exit() { } },
     env: { emscripten_notify_memory_growth() { u8 = new Uint8Array(wasm.memory.buffer); i16 = new Int16Array(wasm.memory.buffer); } },
   });
 
@@ -14,9 +14,8 @@ let wasm;
   i16 = new Int16Array(wasm.memory.buffer);
 }
 
-function err(code) {
-  if (0 > code) throw new Error(`opus: ${load_static_string(u8, wasm.opus_strerror(code))}`);
-}
+function clamp(min, int, max) { const t = int < min ? min : int; return t > max ? max : t; }
+function err(code) { if (0 > code) throw new Error(`opus: ${load_static_string(u8, wasm.opus_strerror(code))}`); else return code; }
 
 function load_static_string(u8, ptr) {
   let s = '';
@@ -33,152 +32,204 @@ function load_static_string(u8, ptr) {
 
 // todo decoder
 // todo pcm stream impl
-// todo export constants
+
+export const ctl = {
+  auto: -1000,
+  bitrate_max: -1,
+  reset_state: 4028,
+
+  signal: {
+    auto: -1000, // default
+    voice: 3001, // best for voice
+    music: 3002, // best for audio/music
+  },
+
+  application: {
+    voip: 2048, // best for voice
+    audio: 2049, // best for audio/music
+    restricted_lowdelay: 2051, // best for low coding delay
+  },
+
+  bandwidth: {
+    auto: -1000,
+    wideband: 1103, // 8 KHz
+    fullband: 1105, // 20 KHz
+    narrowband: 1101, // 4 KHz
+    mediumband: 1102, // 6 KHz
+    superwideband: 1104, // 12 KHz
+  },
+
+  framesize: {
+    5: 5002, // 5ms frames
+    10: 5003, // 10ms frames
+    20: 5004, // 20ms frames
+    40: 5005, // 40ms frames
+    60: 5006, // 60ms frames
+    80: 5007, // 80ms frames
+    100: 5008, // 100ms frames
+    120: 5009, // 120ms frames
+    [2.5]: 5001, // 2.5ms frames
+    arg: 5000, // encode argument
+  },
+
+  set: {
+    vbr: 4006,
+    dtx: 4016,
+    gain: 4034,
+    signal: 4024,
+    bitrate: 4002,
+    bandwidth: 4008,
+    lsb_depth: 4036,
+    complexity: 4010,
+    inband_fec: 4012,
+    application: 4000,
+    max_bandwidth: 4004,
+    vbr_constraint: 4020,
+    force_channels: 4022,
+    packet_loss_perc: 4014,
+    prediction_disabled: 4042,
+    expert_frame_duration: 4040,
+    phase_inversion_disabled: 4046,
+  },
+
+  get: {
+    vbr: 4007,
+    dtx: 4017,
+    gain: 4045,
+    pitch: 4033,
+    signal: 4025,
+    in_dtx: 4049,
+    bitrate: 4003,
+    lsb_depth: 4037,
+    bandwidth: 4009,
+    lookahead: 4027,
+    complexity: 4011,
+    inband_fec: 4013,
+    application: 4001,
+    sample_rate: 4029,
+    final_range: 4031,
+    max_bandwidth: 4005,
+    vbr_constraint: 4021,
+    force_channels: 4023,
+    packet_loss_perc: 4015,
+    prediction_disabled: 4043,
+    last_packet_duration: 4039,
+    expert_frame_duration: 4041,
+    phase_inversion_disabled: 4047,
+  },
+};
+
+const convert = {
+  fc: {
+    1: 1,
+    2: 2,
+    'auto': [ctl.auto],
+    [ctl.auto]: 'auto',
+  },
+
+  s: {
+    [ctl.auto]: 'auto',
+    [ctl.signal.voice]: 'voice',
+    [ctl.signal.music]: 'music',
+  },
+
+  a: {
+    [ctl.application.voip]: 'voip',
+    [ctl.application.audio]: 'audio',
+    [ctl.application.restricted_lowdelay]: 'restricted_lowdelay',
+  },
+
+  b: {
+    [ctl.auto]: 'auto',
+    [ctl.bandwidth.wideband]: 'wideband',
+    [ctl.bandwidth.fullband]: 'fullband',
+    [ctl.bandwidth.narrowband]: 'narrowband',
+    [ctl.bandwidth.mediumband]: 'mediumband',
+    [ctl.bandwidth.superwideband]: 'superwideband',
+  },
+
+  efd: {
+    [ctl.framesize[5]]: 5,
+    [ctl.framesize[10]]: 10,
+    [ctl.framesize[20]]: 20,
+    [ctl.framesize[40]]: 40,
+    [ctl.framesize[60]]: 60,
+    [ctl.framesize[80]]: 80,
+    [ctl.framesize[2.5]]: 2.5,
+    [ctl.framesize[100]]: 100,
+    [ctl.framesize[120]]: 120,
+    [ctl.framesize.arg]: 'arg',
+  },
+}
+
+const pptr = wasm.malloc(2 ** 13);
+const bptr = wasm.malloc(2 ** 14);
 
 export class Encoder {
   #ptr = 0;
-  #pptr = 0;
-  #bptr = 0;
-  #max_packet_size = 2 ** 12;
+  max_opus_size = 2 ** 13;
 
-  constructor({ channels, application, sample_rate, max_packet_size }) {
+  constructor({ channels, application, sample_rate, max_opus_size }) {
     this.channels = channels || 2;
-    this.rate = sample_rate || 48000;
-    this.#max_packet_size = max_packet_size || this.#max_packet_size;
+    application = ctl.application[application || 'audio'];
+    this.max_opus_size = max_opus_size || this.max_opus_size;
     this.#ptr = wasm.malloc(wasm.opus_encoder_get_size(this.channels));
-    application = ({ voip: 2048, audio: 2049, lowdelay: 2051 })[application || 'audio'];
-
-    this.#pptr = wasm.malloc(this.#max_packet_size);
-    this.#bptr = wasm.malloc(this.#max_packet_size);
-    try { err(wasm.opus_encoder_init(this.#ptr, this.rate, this.channels, application)); } catch (e) { throw (this.drop(), e); }
+    try { err(wasm.opus_encoder_init(this.#ptr, sample_rate || 48000, this.channels, application)); } catch (e) { throw (this.drop(), e); }
   }
 
-  drop() { return (wasm.free(this.#ptr), wasm.free(this.#pptr), wasm.free(this.#bptr)); }
+  drop() { return (wasm.free(this.#ptr)); }
+  ctl(cmd, arg) { if (arg == null) return wasm.opus_encoder_ctl_get(this.#ptr, cmd); else return err(wasm.opus_encoder_ctl_set(this.#ptr, cmd, arg)); }
 
   encode(size, buffer) {
-    let l = 0;
-
     if (2 === this.channels) {
-      const ll = buffer.length | 0;
-      for (let o = 0 | 0; o < ll; o++) buffer[o] = buffer[1 + 2 * o] << 8 | buffer[2 * o];
+      const l = buffer.length | 0;
+      for (let o = 0 | 0; o < l; o++) buffer[o] = buffer[1 + 2 * o] << 8 | buffer[2 * o];
     }
 
-    i16.set(buffer, this.#bptr / 2);
-    err(l = wasm.opus_encode(this.#ptr, this.#bptr, size, this.#pptr, this.#max_packet_size));
+    i16.set(buffer, bptr / 2);
+    const l = err(wasm.opus_encode(this.#ptr, bptr, size, pptr, this.max_opus_size));
 
-    return u8.slice(this.#pptr, l + this.#pptr);
+    return u8.slice(pptr, l + pptr);
   }
 
-  get vbr() { return wasm.opus_encoder_ctl_get(this.#ptr, 4007); }
-  get dtx() { return wasm.opus_encoder_ctl_get(this.#ptr, 4049); }
-  get signal() { return wasm.opus_encoder_ctl_get(this.#ptr, 4025); }
-  get bitrate() { return wasm.opus_encoder_ctl_get(this.#ptr, 4003); }
-  get bandwidth() { return wasm.opus_encoder_ctl_get(this.#ptr, 4009); }
-  get lookahead() { return wasm.opus_encoder_ctl_get(this.#ptr, 4027); }
-  get lsb_depth() { return wasm.opus_encoder_ctl_get(this.#ptr, 4037); }
-  get complexity() { return wasm.opus_encoder_ctl_get(this.#ptr, 4011); }
-  get inband_fec() { return wasm.opus_encoder_ctl_get(this.#ptr, 4013); }
-  get application() { return wasm.opus_encoder_ctl_get(this.#ptr, 4001); }
-  get packet_loss() { return wasm.opus_encoder_ctl_get(this.#ptr, 4015); }
-  get sample_rate() { return wasm.opus_encoder_ctl_get(this.#ptr, 4029); }
-  get max_bandwidth() { return wasm.opus_encoder_ctl_get(this.#ptr, 4005); }
-  get vbr_constraint() { return wasm.opus_encoder_ctl_get(this.#ptr, 4021); }
-  get force_channels() { return wasm.opus_encoder_ctl_get(this.#ptr, 4023); }
-  get prediction_disabled() { return wasm.opus_encoder_ctl_get(this.#ptr, 4043); }
-  get expert_frame_duration() { return wasm.opus_encoder_ctl_get(this.#ptr, 4041); }
-  get phase_inversion_disabled() { return wasm.opus_encoder_ctl_get(this.#ptr, 4047); }
+  // https://www.opus-codec.org/docs/opus_api-1.3.1/group__opus__genericctls.html
+  reset() { this.ctl(ctl.reset_state); }
+  get in_dtx() { return !!this.ctl(ctl.get.in_dtx); }
+  get sample_rate() { return this.ctl(ctl.get.sample_rate); }
+  get bandwidth() { return convert.b[this.ctl(ctl.get.bandwidth)]; }
+  get phase_inversion_disabled() { return !!this.ctl(ctl.get.phase_inversion_disabled); }
+  set phase_inversion_disabled(bool) { this.ctl(ctl.set.phase_inversion_disabled, bool ? 1 : 0); }
 
-  reset() { err(wasm.opus_encoder_ctl_set(this.#ptr, 4028)); }
-  set vbr(bool) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4006, bool ? 1 : 0)); }
-  set dtx(bool) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4016, bool ? 1 : 0)); }
-  set inband_fec(bool) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4012, bool ? 1 : 0)); }
-  set vbr_constraint(bool) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4020, bool ? 1 : 0)); }
-  set prediction_disabled(bool) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4042, bool ? 1 : 0)); }
-  set phase_inversion_disabled(bool) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4046, bool ? 1 : 0)); }
-  set lsb_depth(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4036, Math.min(Math.max(int | 0, 8), 24))); }
-  set complexity(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4010, Math.min(Math.max(int | 0, 0), 10))); }
-  set signal(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4024, Math.min(Math.max(int | 0, 3001), 3002))); }
-  set packet_loss(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4014, Math.min(Math.max(int | 0, 0), 100))); }
-  set bitrate(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4002, Math.min(Math.max(int | 0, 500), 512000))); }
-  set force_channels(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4022, Math.min(Math.max(int | 0, 1), 2))); }
-  set bandwidth(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4008, Math.min(Math.max(int | 0, 1101), 1105))); }
-  set application(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4000, Math.min(Math.max(int | 0, 2049), 2051))); }
-  set max_bandwidth(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4004, Math.min(Math.max(int | 0, 1101), 1105))); }
-  set expert_frame_duration(int) { err(wasm.opus_encoder_ctl_set(this.#ptr, 4040, Math.min(Math.max(int | 0, 5000), 5009))); }
+  // https://www.opus-codec.org/docs/opus_api-1.3.1/group__opus__encoderctls.html
+  get vbr() { return !!this.ctl(ctl.get.vbr); }
+  get dtx() { return !!this.ctl(ctl.get.dtx); }
+  get bitrate() { return this.ctl(ctl.get.bitrate); }
+  get lookahead() { return this.ctl(ctl.get.lookahead); }
+  get lsb_depth() { return this.ctl(ctl.get.lsb_depth); }
+  get complexity() { return this.ctl(ctl.get.complexity); }
+  get inband_fec() { return !!this.ctl(ctl.get.inband_fec); }
+  get signal() { return convert.s[this.ctl(ctl.get.signal)]; }
+  get packet_loss() { return this.ctl(ctl.get.packet_loss_perc); }
+  get vbr_constraint() { return !!this.ctl(ctl.get.vbr_constraint); }
+  get application() { return convert.a[this.ctl(ctl.get.application)]; }
+  get max_bandwidth() { return convert.b[this.ctl(ctl.get.max_bandwidth)]; }
+  get force_channels() { return convert.fc[this.ctl(ctl.get.force_channels)]; }
+  get prediction_disabled() { return !!this.ctl(ctl.get.prediction_disabled); }
+  get expert_frame_duration() { return covert.efd[this.ctl(ctl.get.expert_frame_duration)]; }
+
+  set vbr(bool) { this.ctl(ctl.set.vbr, bool ? 1 : 0); }
+  set dtx(bool) { this.ctl(ctl.set.dtx, bool ? 1 : 0); }
+  set signal(arg) { this.ctl(ctl.set.signal, ctl.signal[arg]); }
+  set inband_fec(bool) { this.ctl(ctl.set.inband_fec, bool ? 1 : 0); }
+  set lsb_depth(int) { this.ctl(ctl.set.lsb_depth, clamp(8, int, 24)); }
+  set bandwidth(arg) { this.ctl(ctl.set.bandwidth, ctl.bandwidth[arg]); }
+  set complexity(int) { this.ctl(ctl.set.complexity, clamp(0, int, 10)); }
+  set vbr_constraint(bool) { this.ctl(ctl.set.vbr_constraint, bool ? 1 : 0); }
+  set application(arg) { this.ctl(ctl.set.application, ctl.application[arg]); }
+  set force_channels(arg) { this.ctl(ctl.set.force_channels, convert.fc[arg]); }
+  set packet_loss(int) { this.ctl(ctl.set.packet_loss_perc, clamp(0, int, 100)); }
+  set prediction_disabled(bool) { this.ctl(ctl.set.prediction_disabled, bool ? 1 : 0); }
+  set expert_frame_duration(arg) { this.ctl(ctl.set.expert_frame_duration, ctl.framesize[arg]) };
+  set max_bandwidth(arg) { this.ctl(ctl.set.max_bandwidth, ctl.bandwidth[arg === 'auto' ? 'fullband' : arg]); }
+  set bitrate(arg) { this.ctl(ctl.set.bitrate, arg === 'max' ? ctl.bitrate_max : (arg === 'auto' ? ctl.auto : clamp(500, arg | 0, 512000))); }
 }
-
-// #define OPUS_SET_APPLICATION_REQUEST         4000
-// #define OPUS_GET_APPLICATION_REQUEST         4001
-// #define OPUS_SET_BITRATE_REQUEST             4002
-// #define OPUS_GET_BITRATE_REQUEST             4003
-// #define OPUS_SET_MAX_BANDWIDTH_REQUEST       4004
-// #define OPUS_GET_MAX_BANDWIDTH_REQUEST       4005
-// #define OPUS_SET_VBR_REQUEST                 4006
-// #define OPUS_GET_VBR_REQUEST                 4007
-// #define OPUS_SET_BANDWIDTH_REQUEST           4008
-// #define OPUS_GET_BANDWIDTH_REQUEST           4009
-// #define OPUS_SET_COMPLEXITY_REQUEST          4010
-// #define OPUS_GET_COMPLEXITY_REQUEST          4011
-// #define OPUS_SET_INBAND_FEC_REQUEST          4012
-// #define OPUS_GET_INBAND_FEC_REQUEST          4013
-// #define OPUS_SET_PACKET_LOSS_PERC_REQUEST    4014
-// #define OPUS_GET_PACKET_LOSS_PERC_REQUEST    4015
-// #define OPUS_SET_DTX_REQUEST                 4016
-// #define OPUS_GET_DTX_REQUEST                 4017
-// #define OPUS_SET_VBR_CONSTRAINT_REQUEST      4020
-// #define OPUS_GET_VBR_CONSTRAINT_REQUEST      4021
-// #define OPUS_SET_FORCE_CHANNELS_REQUEST      4022
-// #define OPUS_GET_FORCE_CHANNELS_REQUEST      4023
-// #define OPUS_SET_SIGNAL_REQUEST              4024
-// #define OPUS_GET_SIGNAL_REQUEST              4025
-// #define OPUS_GET_LOOKAHEAD_REQUEST           4027
-// /* #define OPUS_RESET_STATE 4028 */
-// #define OPUS_GET_SAMPLE_RATE_REQUEST         4029
-// #define OPUS_GET_FINAL_RANGE_REQUEST         4031
-// #define OPUS_GET_PITCH_REQUEST               4033
-// #define OPUS_SET_GAIN_REQUEST                4034
-// #define OPUS_GET_GAIN_REQUEST                4045 /* Should have been 4035 */
-// #define OPUS_SET_LSB_DEPTH_REQUEST           4036
-// #define OPUS_GET_LSB_DEPTH_REQUEST           4037
-// #define OPUS_GET_LAST_PACKET_DURATION_REQUEST 4039
-// #define OPUS_SET_EXPERT_FRAME_DURATION_REQUEST 4040
-// #define OPUS_GET_EXPERT_FRAME_DURATION_REQUEST 4041
-// #define OPUS_SET_PREDICTION_DISABLED_REQUEST 4042
-// #define OPUS_GET_PREDICTION_DISABLED_REQUEST 4043
-// /* Don't use 4045, it's already taken by OPUS_GET_GAIN_REQUEST */
-// #define OPUS_SET_PHASE_INVERSION_DISABLED_REQUEST 4046
-// #define OPUS_GET_PHASE_INVERSION_DISABLED_REQUEST 4047
-// #define OPUS_GET_IN_DTX_REQUEST              4049
-
-// /* Values for the various encoder CTLs */
-// #define OPUS_AUTO                           -1000 /**<Auto/default setting @hideinitializer*/
-// #define OPUS_BITRATE_MAX                       -1 /**<Maximum bitrate @hideinitializer*/
-
-// /** Best for most VoIP/videoconference applications where listening quality and intelligibility matter most
-//  * @hideinitializer */
-// #define OPUS_APPLICATION_VOIP                2048
-// /** Best for broadcast/high-fidelity application where the decoded audio should be as close as possible to the input
-//  * @hideinitializer */
-// #define OPUS_APPLICATION_AUDIO               2049
-// /** Only use when lowest-achievable latency is what matters most. Voice-optimized modes cannot be used.
-//  * @hideinitializer */
-// #define OPUS_APPLICATION_RESTRICTED_LOWDELAY 2051
-
-// #define OPUS_SIGNAL_VOICE                    3001 /**< Signal being encoded is voice */
-// #define OPUS_SIGNAL_MUSIC                    3002 /**< Signal being encoded is music */
-// #define OPUS_BANDWIDTH_NARROWBAND            1101 /**< 4 kHz bandpass @hideinitializer*/
-// #define OPUS_BANDWIDTH_MEDIUMBAND            1102 /**< 6 kHz bandpass @hideinitializer*/
-// #define OPUS_BANDWIDTH_WIDEBAND              1103 /**< 8 kHz bandpass @hideinitializer*/
-// #define OPUS_BANDWIDTH_SUPERWIDEBAND         1104 /**<12 kHz bandpass @hideinitializer*/
-// #define OPUS_BANDWIDTH_FULLBAND              1105 /**<20 kHz bandpass @hideinitializer*/
-
-// #define OPUS_FRAMESIZE_ARG                   5000 /**< Select frame size from the argument (default) */
-// #define OPUS_FRAMESIZE_2_5_MS                5001 /**< Use 2.5 ms frames */
-// #define OPUS_FRAMESIZE_5_MS                  5002 /**< Use 5 ms frames */
-// #define OPUS_FRAMESIZE_10_MS                 5003 /**< Use 10 ms frames */
-// #define OPUS_FRAMESIZE_20_MS                 5004 /**< Use 20 ms frames */
-// #define OPUS_FRAMESIZE_40_MS                 5005 /**< Use 40 ms frames */
-// #define OPUS_FRAMESIZE_60_MS                 5006 /**< Use 60 ms frames */
-// #define OPUS_FRAMESIZE_80_MS                 5007 /**< Use 80 ms frames */
-// #define OPUS_FRAMESIZE_100_MS                5008 /**< Use 100 ms frames */
-// #define OPUS_FRAMESIZE_120_MS                5009 /**< Use 120 ms frames */
