ed493a131f43580e1590dcee664b2ab1795c47b8
evanwashere <evan@tsu.sh>
2021-10-28 15:11:15 -0400

    add imagescript module

---
+++ src/imagescript/imagescript.js
@@ -0,0 +1,46 @@
+let wasm;
+
+{
+  const module = new WebAssembly.Module(WASM_BYTES);
+  const instance = new WebAssembly.Instance(module);
+
+  wasm = instance.exports;
+}
+
+class mem {
+  static length() { return wasm.wlen(); }
+  static token() { return wasm.wtoken(); }
+  static alloc(size) { return wasm.walloc(size); }
+  static free(ptr, size) { return wasm.wfree(ptr, size); }
+  static u8(ptr, size) { return new Uint8Array(wasm.memory.buffer, ptr, size); }
+  static u32(ptr, size) { return new Uint32Array(wasm.memory.buffer, ptr, size); }
+  static load(ptr) { return new Uint8Array(wasm.memory.buffer, ptr, wasm.wlen()); }
+  static store(u8) { const ptr = wasm.walloc(u8.length); return (new Uint8Array(wasm.memory.buffer, ptr, u8.length).set(u8), ptr); }
+  static gc(f) { return !('FinalizationRegistry' in globalThis) ? { delete(_) { }, add(_, __) { } } : { r: new FinalizationRegistry(f), delete(k) { this.r.unregister(k); }, add(k, v) { this.r.register(k, v, k); } }; }
+
+  static copy_and_free(ptr, size) {
+    let slice = mem.u8(ptr, size).slice();
+    return (wasm.wfree(ptr, size), slice);
+  }
+}
+
+const gc = mem.gc(ptr => wasm.free(ptr));
+
+export class framebuffer {
+  constructor(ptr) { gc.add(this, this.ptr = ptr); }
+  static new(width, height) { return new this(wasm.new(width, height)); }
+  static from(width, height, buffer) { const fb = this.new(width, height); return (fb.buffer.set(buffer), fb); }
+
+  get width() { return wasm.width(this.ptr); }
+  get height() { return wasm.height(this.ptr); }
+  get buffer() { return mem.load(wasm.buffer(this.ptr)); }
+  drop() { gc.delete((this.ptr = wasm.free(this.ptr), this)); }
+}
+
+const rt = {
+  cubic: 2,
+  linear: 1,
+  neareast: 0,
+};
+
+export function resize(fb, type, width, height) { return new framebuffer(wasm.resize(fb.ptr, rt[type], width, height)); }
